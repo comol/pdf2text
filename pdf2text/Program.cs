023e17d7709cbf2caca255857d924f74f64e7abb
comol <comol@mail.ru>
2022-11-16 20:44:16 +0300

    Add project files.

---
+++ pdf2text/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using UglyToad.PdfPig;
+using UglyToad.PdfPig.Content;
+
+string resulttext = "";
+
+using (PdfDocument document = PdfDocument.Open(Environment.GetCommandLineArgs()[1]))
+{
+    
+    foreach (Page page in document.GetPages())
+    {
+        resulttext = resulttext + page.Text;
+    }
+}
+
+File.WriteAllText(Environment.GetCommandLineArgs()[2], resulttext);
